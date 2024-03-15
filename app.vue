be8e26c85c1e09a2b1704215c6067800546454a3
MikeD579 <michaelmason579@gmail.com>
2024-03-15 07:56:57 -0500

    add lorem ipsum

--- app.vue
+++ app.vue
@@ -1,9 +1,14 @@
 <template>
-  <AppLayout>
+  <NuxtLayout>
+    <AppLayout>
     <h1>
       Welcome To my website!
     </h1>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+    </p>
   </AppLayout>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
--- components/App/AppHeader.vue
+++ components/App/AppHeader.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="header">
-        <a href="#" class="name">Michael Dominguez</a>
         <nav>
-            <li>Home</li>
-            <li>About</li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
         </nav>
     </div>
 </template>
@@ -11,7 +10,7 @@
 <style scoped>
 .header {
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     padding: 0 2rem;
 }
 .name {
