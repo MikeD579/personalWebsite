968dbdc637925038af6af801d1f40697d17f766c
MikeD579 <michaelmason579@gmail.com>
2024-03-16 11:44:39 -0500

    change layout

--- components/App/AppFooter.vue
+++ components/App/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="footer">
         footer here
     </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="app">
         <AppHeader />
         <main>
             <slot></slot>
